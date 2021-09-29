4b7a552429008d7752b6fa718ddb3e1a804afd63
Egor <egorgrebenn@gmail.com>
2021-09-29 11:35:47 +0300

    add day2.2

---
+++ day2/day2.2/Author.java
@@ -0,0 +1,37 @@
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        System.out.printf(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
---
+++ day2/day2.2/Loader.java
@@ -0,0 +1,5 @@
+public class Loader {
+    public static void main(String[] args) {
+        Author author = new Author("Egor","blabla@yahoo.com", 'M');
+    }
+}
